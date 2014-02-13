8a1b62c46d81f45373f7d7442699808073fd493c
George Leontiev <folone@gmail.com>
2014-02-13 17:58:37 +0100

    Problems with null.

--- test/files/run/dottype.scala
+++ test/files/run/dottype.scala
@@ -49,5 +49,36 @@ object Test {
     // println(f(5))
     println((g(1), g(5), g(7)))
   }
+  /*
+scala> val x: null.type = null
+<console>:1: error: identifier expected but 'null' found.
+       val x: null.type = null
+              ^
+
+scala> final val x = null
+x: null.type = null
+
+scala> val y: x.type = 4
+<console>:8: error: type mismatch;
+ found   : 4.type (with underlying type Int)
+ required: null.type
+       val y: x.type = 4
+                       ^
+
+scala> val y: x.type = null
+y: null.type = null
+
+scala> def tst(x: Int) = x match {
+     | case _: 7.type => 1
+     | case _: y.type => 2
+     | case _ => 3
+     | }
+<console>:11: error: scrutinee is incompatible with pattern type;
+ found   : null.type (with underlying type Null)
+ required: Int
+       case _: y.type => 2
+                ^
+java.lang.IllegalArgumentException: requirement failed: expandTypes(<notype>, List(Null), <none>)
+   */
 
 }
