0fefd01407c51805f8656c4241d0d927c63850c3
George Leontiev <folone@gmail.com>
2014-06-25 10:10:01 +0200

    Make .type optional for literals.

--- src/compiler/scala/tools/nsc/ast/parser/Parsers.scala
+++ src/compiler/scala/tools/nsc/ast/parser/Parsers.scala
@@ -1098,8 +1098,10 @@ self =>
         val tok = in.token
         if (tok != BACKQUOTED_IDENT && tok != IDENTIFIER) {
           val lit = literal(false)
-          accept(DOT)
-          accept(TYPE)
+          if (in.token == DOT && lookingAhead { in.token == TYPE }) {
+            accept(DOT)
+            accept(TYPE)
+          }
           return atPos(start)(new SingletonTypeTree(lit) { override val isLiteral = true })
         }
         val name = ident()
