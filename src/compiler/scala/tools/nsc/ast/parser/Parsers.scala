071367c811d46555573d87b1ecc0ec566d63cbc1
Adriaan Moors <adriaan.moors@typesafe.com>
2014-10-24 14:32:41 +0200

    [WIP] SIP-23 Parse `Literal` as `SimpleType`
    This requires -Xexperimental in 2.11.x.
    
    Update spec.
    
    TODO:
     - test!
     - include SIP as spec addendum
     - don't predicate on -Xexperimental -- introduce -Xsip:23?
    
    Notes:
     - `()` is not a literal, neither are `Symbol`s

--- src/compiler/scala/tools/nsc/ast/parser/Parsers.scala
+++ src/compiler/scala/tools/nsc/ast/parser/Parsers.scala
@@ -339,6 +339,8 @@ self =>
     }
     private def inScalaRootPackage = inScalaPackage && currentPackage == "scala"
 
+    // 42.type aka SIP-23
+    private[this] lazy val parseLiteralSingletonTypes = settings.Xexperimental.value
 
     def parseStartRule: () => Tree
 
@@ -676,7 +678,7 @@ self =>
 
     def isExprIntro: Boolean = isExprIntroToken(in.token)
 
-    def isTypeIntroToken(token: Token): Boolean = (sip23 && isLiteralToken(token)) || (token match {
+    def isTypeIntroToken(token: Token): Boolean = (parseLiteralSingletonTypes && isLiteralToken(token)) || (token match {
       case IDENTIFIER | BACKQUOTED_IDENT | THIS |
            SUPER | USCORE | LPAREN | AT => true
       case _ => false
@@ -933,7 +935,7 @@ self =>
        *                     |  SimpleType `#' Id
        *                     |  StableId
        *                     |  Path `.' type
-       *                     |  Literal
+       *                     |  Literal [`.' type]
        *                     |  `(' Types `)'
        *                     |  WildcardType
        *  }}}
@@ -941,9 +943,29 @@ self =>
       def simpleType(): Tree = {
         val start = in.offset
         simpleTypeRest(in.token match {
-          case LPAREN   => atPos(start)(makeTupleType(inParens(types())))
-          case USCORE   => wildcardType(in.skipToken())
-          case tok if sip23 && isLiteralToken(tok) => atPos(start){SingletonTypeTree(literal())} // SIP-23
+          case LPAREN =>
+            atPos(start)(makeTupleType(inParens(types())))
+          case LBRACKET =>
+            atPos(start) {
+              val ts = typeParamClauseOpt(freshTypeName("typelambda"), null)
+              if (in.token == ARROW) {
+                in.skipToken()
+                makeTypeLambdaTypeTree(ts, typ())
+              } else {
+                syntaxError("`=>' expected", skipIt = false)
+                errorTypeTree
+              }
+            }
+          case USCORE =>
+            wildcardType(in.skipToken())
+          case tok if sip23 && isLiteralToken(tok) => // SIP-23
+            // allowing an optional `.type` suffix:
+            val lit = literal()
+            if (in.token == DOT && lookingAhead { in.token == TYPE }) {
+              accept(DOT)
+              accept(TYPE)
+            }
+            atPos(start)(SingletonTypeTree(literal()))
           case _ =>
             path(thisOK = false, typeOK = true) match {
               case r @ SingletonTypeTree(_) => r
