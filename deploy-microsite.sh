25de9b680c4e8eaa9433192e5087bed589ffd24a
Lars Hupel <lars.hupel@mytum.de>
2016-10-12 20:52:14 +0200

    use after_success instead of deploy; otherwise Jekyll disappears

--- deploy-microsite.sh
+++ deploy-microsite.sh
@@ -2,10 +2,16 @@
 
 set -e
 
-GIT_EMAIL="bot@typelevel.org"
-GIT_NAME="Typelevel Bot"
+if [ "$TRAVIS_BRANCH" = "typelevel-readme" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
+  GIT_EMAIL="bot@typelevel.org"
+  GIT_NAME="Typelevel Bot"
 
-git config --global user.name "$GIT_NAME"
-git config --global user.email "$GIT_EMAIL"
+  git config --global user.name "$GIT_NAME"
+  git config --global user.email "$GIT_EMAIL"
+  git config --global push.default simple
 
-sbt publishMicrosite
+  sbt publishMicrosite
+else
+  echo "Skipping deployment!"
+fi
+
