29c794400025e778691388cbcc873a990f44fc4d
Miles Sabin <miles@milessabin.com>
2016-10-25 17:23:01 +0100

    More script fixes.

--- try-typelevel-scala.sh
+++ try-typelevel-scala.sh
@@ -12,5 +12,3 @@ test -e ~/.coursier/coursier || \
   org.typelevel:scala-reflect:2.11.8 \
   -- --predef 'repl.compiler.settings.YpartialUnification.value = true
     repl.compiler.settings.YliteralTypes.value = true' < /dev/tty
-#  -- --predef 'repl.compiler.settings.YpartialUnification.value = true' < /dev/tty
-#  -- --predef 'repl.compiler.settings.YliteralTypes.value = true' < /dev/tty
