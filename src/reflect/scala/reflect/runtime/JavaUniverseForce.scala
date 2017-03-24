9d2f0ab260fe4a36f2274d64bdac5cff59fa8216
Miles Sabin <miles@milessabin.com>
2017-03-24 13:28:06 +0000

    Faster resolution of inductive implicits.

--- src/reflect/scala/reflect/runtime/JavaUniverseForce.scala
+++ src/reflect/scala/reflect/runtime/JavaUniverseForce.scala
@@ -389,6 +389,7 @@ trait JavaUniverseForce { self: runtime.JavaUniverse  =>
     definitions.ScalaStrictFPAttr
     definitions.SwitchClass
     definitions.TailrecClass
+    definitions.InductiveClass
     definitions.VarargsClass
     definitions.uncheckedStableClass
     definitions.uncheckedVarianceClass
